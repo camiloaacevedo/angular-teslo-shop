70a2f0f2a9ba2fddda0f8b4770133b7f1dc49e4e
camiloaacevedo <ccuevasa@uninorte.edu.co>
2025-07-18 22:00:30 -0500

    .

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -2,7 +2,9 @@ import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
+
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
@@ -11,11 +13,26 @@ import { PaginationComponent } from '@shared/components/pagination/pagination.co
 })
 export class HomePageComponent {
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  // activatedRoute = inject(ActivatedRoute);
+
+  // currentPage = toSignal(
+  //   this.activatedRoute.queryParamMap.pipe(
+  //     map((params) => (params.get('page') ? +params.get('page')! : 1)),
+  //     map((page) => (isNaN(page) ? 1 : page))
+  //   ),
+  //   {
+  //     initialValue: 1,
+  //   }
+  // );
 
   productsResource = rxResource({
-    params: () => ({}),
+    params: () => ({ page: this.paginationService.currentPage() - 1 }),
     stream: ({ params }) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        offset: params.page * 9,
+      });
     },
   });
 }
