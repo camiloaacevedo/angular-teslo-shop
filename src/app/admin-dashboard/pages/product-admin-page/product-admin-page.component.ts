635ea44c1d8654d58f94eb3a22f6469d503775a8
camiloaacevedo <ccuevasa@uninorte.edu.co>
2025-07-22 00:15:03 -0500

    dashboard

---
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-admin-page',
+  imports: [],
+  templateUrl: './product-admin-page.component.html',
+})
+export class ProductAdminPageComponent { }
